ef70c135d72ad81d4948727ec4db1c81f6f92330
QunFan <allenfan1012@gmail.com>
2022-08-05 15:35:21 +1000

    commit tree 3981ead40875559c2591691b8b34ccc7010fbdc7

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let thankYouMessage = []
+    for (let i=0; i <names.length; i++) {
+        thankYouMessage.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessage;
+}
+
+let countDown = function(num){
+
+    while (num > 0) {
+        console.log (num);
+        num --;
+    }
+    console.log(num);
+}
